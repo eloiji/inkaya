b2011e821f1679da66d37af1ef86a607a93d8f4e
eloiji <eloisajocsoniting@gmail.com>
2025-10-08 11:28:12 +0800

    feat: Added default pages when navigation link is clicked

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,7 @@
+export default function Contact() {
+  return (
+    <section className="flex min-h-full flex-col items-center justify-center p-24">
+      <h1 className="text-4xl font-bold">Contact Page</h1>
+    </section>
+  );
+}
